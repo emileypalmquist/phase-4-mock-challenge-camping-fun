afdf006ed4f16d92bd0e0b6b202823f8287a3ed2
emileypalmquist <emiley.palmquist@icloud.com>
2022-03-18 12:38:17 -0700

    review solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :signups, only: [:create]
+  # post "/signups", to: "signups#create"
+  resources :campers, only: [:index, :show, :create]
+  # get "/campers", to: "campers#index"
+  # get "/campers/:id", to: "campers#show"
+  # post "/campers", to: "campers#create"
+  resources :activities, only: [:index, :destroy]
+  # get "/activities", to: "activities#index"
+  # delete "/activities/:id", to: "activities#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
